0067c94e719eb302ea8b5c524bdadd27dd544009
akubara <akubaradev@gmail.com>
2021-02-21 01:36:48 +0300

    Del toast store

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,7 @@ import { persistState } from '@storeon/localstorage';
 import { createStoreon } from 'storeon';
 
 import main from './main';
-import toast from './toast';
 
-const store = createStoreon([main, toast, persistState(['tags', 'sites'])]);
+const store = createStoreon([main, persistState(['tags', 'sites'])]);
 
 export default store;
--- src/store/toast.js
+++
@@ -1,8 +0,0 @@
-export default function toast(store) {
-  store.on('@init', () => ({
-    toastIsShow: false,
-  }));
-
-  store.on('toast/show', () => ({ toastIsShow: true }));
-  store.on('toast/hide', () => ({ toastIsShow: false }));
-}
