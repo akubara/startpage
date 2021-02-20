fae6c51f850ccc007eade3c0d790feb5b53c7774
akubara <akubaradev@gmail.com>
2021-02-20 23:55:49 +0300

    Del utils

--- src/utils/getDifferenceBetweenArrays.js
+++
@@ -1,7 +0,0 @@
-export default function getDifferenceBetweenArrays(oldArray, newArray, key = null) {
-  if (key) {
-    return oldArray.filter((item) => !newArray.some((newItem) => newItem[key] === item[key]));
-  }
-
-  return oldArray.filter((item) => !newArray.includes(item));
-}
