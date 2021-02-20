b118aa0d33514491a5b7690aa127be1b63088e6f
akubara <akubaradev@gmail.com>
2021-02-21 01:14:06 +0300

    Fix style import

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,8 @@ import { useStoreon } from 'storeon/preact';
 
 import Input from './Input';
 
+import '../styles/form.css';
+
 export default function Form({ dispatchKey, payloadPattern, placeholder, mini, max = 256 }) {
   const [value, setValue] = useState('');
   const { dispatch } = useStoreon();
