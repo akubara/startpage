6f97d1fad6ecadf468ee8115d204dc45ff728683
akubara <akubaradev@gmail.com>
2021-02-21 01:11:15 +0300

    Del projects

--- src/store/projects.js
+++
@@ -1,5 +0,0 @@
-export default function projects(store) {
-  store.on('@init', () => ({ projects: [] }));
-
-  store.on('projects/add', ({ projects }, project) => ({ projects: projects.concat([project]) }));
-}
