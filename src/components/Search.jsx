49299cde8348757543fd878fce4b953a83bfc84f
akubara <akubaradev@gmail.com>
2021-02-21 03:18:22 +0300

    Del search

--- src/components/Search.jsx
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact';
-
-export default function Search() {
-  return (
-    <form action="http://google.com/search" method="get">
-      <input type="search" name="q" placeholder="google search" />
-    </form>
-  );
-}
