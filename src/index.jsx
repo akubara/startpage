c4f65ef98a6bc80bcd3cb76a632c2c13352a55d8
Ilya Zaru <36209593+akubara@users.noreply.github.com>
2021-02-23 01:11:16 +0300

    Del duplicate import

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import { h, render } from 'preact';
 import { StoreContext } from 'storeon/preact';
 
-import 'react-toastify/dist/ReactToastify.css';
-
 import './styles/tailwind.css';
 import App from './App';
 import store from './store';
